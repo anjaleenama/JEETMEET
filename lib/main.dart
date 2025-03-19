5b7d7e3eed6301e956232d2b4f0ad72247698f04
vismaya <vismyaprakash2004@gmail.com>
2025-03-19 12:17:17 +0530

    profile page updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:jeet_meet/drawyer.dart';
+import 'package:jeet_meet/loginpage.dart';
+import 'package:jeet_meet/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyDrawerPage(),
+      home: MyProfileScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
